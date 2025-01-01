ace995e05f9ba877cd987f6aadead613de935f63
Jeremiah <jeremiah@pop-os.localdomain>
2025-01-01 12:01:55 -0700

    modified palworld script so that it closes the current server first. This is handy for windows cron jobs

--- palworld_start.bat
+++ palworld_start.bat
@@ -1,6 +1,40 @@
 @echo off
-echo Checking for PalWorld Server updates...
-C:\steamcmd\steamcmd.exe +login anonymous +app_update 2394010 validate +quit
-echo Launching PalWorld server
+set LOGFILE=C:\Scripts\palworld_log.txt
+
+echo ----------------------- [%date%] --------------------- >> %LOGFILE%
+
+:: Log the start of the process
+echo [%date% %time%] - Stopping PalWorld server... >> %LOGFILE%
+
+:: Force kill processes by full path using PowerShell
+echo [%date% %time%] - Searching for PalWorld server process... >> %LOGFILE%
+powershell -Command "Get-Process | Where-Object {$_.Path -eq 'C:\steamcmd\steamapps\common\PalServer\Pal\Binaries\Win64\PalServer-Win64-Shipping-Cmd.exe'} | Stop-Process -Force" >> %LOGFILE% 2>&1
+
+:: Pause briefly to ensure processes have been terminated
+timeout /t 5 /nobreak >nul
+
+:: Double-check if processes are still running using PowerShell
+echo [%date% %time%] - Verifying PalWorld server is stopped... >> %LOGFILE%
+powershell -Command "if (Get-Process | Where-Object {$_.Path -eq 'C:\steamcmd\steamapps\common\PalServer\Pal\Binaries\Win64\PalServer-Win64-Shipping-Cmd.exe'}) { exit 1 }" 
+if %errorlevel% neq 0 (
+    echo [%date% %time%] - ERROR: PalServer-Win64-Shipping-Cmd.exe is still running! >> %LOGFILE%
+    exit /b 1
+)
+
+:: Proceed if process is confirmed stopped
+echo [%date% %time%] - PalWorld server stopped successfully. >> %LOGFILE%
+
+:: Check for updates
+echo [%date% %time%] - Checking for PalWorld Server updates... >> %LOGFILE%
+C:\steamcmd\steamcmd.exe +login anonymous +app_update 2394010 validate +quit >> %LOGFILE% 2>&1
+
+if %errorlevel% neq 0 (
+    echo [%date% %time%] - Update failed with error code %errorlevel%. >> %LOGFILE%
+    exit /b %errorlevel%
+)
+
+:: Launch the server
+echo [%date% %time%] - Launching PalWorld server... >> %LOGFILE%
 cd C:\steamcmd\steamapps\common\PalServer
-PalServer.exe -ServerName="TeagueWorld" -port=8211 -players=4 -useperfthreads -NoAsyncLoadingThread -UseMultithreadForDS
+start "" /B PalServer.exe -ServerName="TeagueWorld" -port=8211 -players=4 -useperfthreads -NoAsyncLoadingThread -UseMultithreadForDS
+echo [%date% %time%] - Server started successfully. >> %LOGFILE%
